fd8c4829f23f068148d579ec1e461042bc91ebe3
Daker515 <erosjejexd3@gmail.com>
2024-07-19 02:06:09 -0500

    4 commit

--- src/main/java/com/foodappbackend/foodapp/controller/ProductoController.java
+++ src/main/java/com/foodappbackend/foodapp/controller/ProductoController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/productos")
 @AllArgsConstructor
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class ProductoController {
 
     @Autowired
--- src/main/java/com/foodappbackend/foodapp/controller/UsuarioController.java
+++ src/main/java/com/foodappbackend/foodapp/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/usuarios")
 @AllArgsConstructor
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class UsuarioController {
 
 
